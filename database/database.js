7ec6d6c210d9b6fdace249abb17aeea89242e1b7
Relton Fidelis <contato.reltonfidelis@gmail.com>
2021-11-18 21:10:47 -0300

    alter gitignore file

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('database', 'db_user', 'password', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
